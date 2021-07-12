1d9cd13a2d344faf76bb61ce3fbfcc1583ca6511
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-12 20:06:54 +0530

    ADDED: program to add user details by adding first name

--- src/com/bridgelabz/userRegistration.java
+++ src/com/bridgelabz/userRegistration.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 
-public interface userRegistration {
+public interface userRegistration 
+{
 		public boolean firstNameinfo(String firstName);
-		public boolean lastNameinfo(String lastName);
-
 }
--- src/com/bridgelabz/userRegistrationImpl.java
+++ src/com/bridgelabz/userRegistrationImpl.java
@@ -1,13 +1,10 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class userRegistrationImpl implements userRegistration
 {
-
-	@Override
 	public boolean firstNameinfo(String firstName)
 	{
 		String firstNameinfo = "^[A-Z]{1}[a-z]{3,20}$";
